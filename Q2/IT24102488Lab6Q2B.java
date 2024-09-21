692ac7ab7bc5baee888cc58be52cd69cba3fae7f
IT24102488 <it24102488@my.sliit.lk>
2024-09-21 13:47:07 +0530

    Q2B
    Q2B

---
+++ Q2/IT24102488Lab6Q2B.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+    public class IT24102488Lab6Q2B{
+        public static void main(String[] args) {
+
+
+
+
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("enter number 1 :");
+            int num1=input.nextInt();
+
+
+            System.out.print("enter number 2 :");
+            int num2=input.nextInt();
+
+
+            System.out.print("enter number 3 :");
+            int num3=input.nextInt();
+
+
+            System.out.print("enter number 4 :");
+            int num4=input.nextInt();
+
+
+            System.out.print("enter number 5 :");
+            int num5=input.nextInt();
+
+
+            System.out.print("enter number 6 :");
+            int num6=input.nextInt();
+
+
+            System.out.print("enter number 7 :");
+            int num7=input.nextInt();
+
+
+            System.out.print("enter number 8 :");
+            int num8=input.nextInt();
+
+
+            System.out.print("enter number 9 :");
+            int num9=input.nextInt();
+
+
+            System.out.print("enter number 10 :");
+            int num10=input.nextInt();
+
+            System.out.print("The numbers  you enterd are :");
+            System.out.print(num1+" "+num2+" "+num3+" "+num4+" "+num5+" "+num6+" "+num7+" "+num8+" "+num9+" "+num10);
+
+
+
+
+        }
+    }
+
