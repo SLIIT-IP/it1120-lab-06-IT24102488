bee3633d90961370ba0ba0ea3fb5cc6d2a5e9e89
IT24102488 <it24102488@my.sliit.lk>
2024-09-21 13:46:37 +0530

    Q2
    Q2

---
+++ Q2/IT24102488Lab6Q2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class IT24102488Lab6Q2
+{
+public static void main (String[] args){
+int count=1;
+while(count<=10){
+System.out.print(count+" ")
+count=count+1;
+}
+}
+}
