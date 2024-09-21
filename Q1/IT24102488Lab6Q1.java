8ba0efaeb408a5eb00afdcd5ba7854ce1ca3c7da
IT24102488 <it24102488@my.sliit.lk>
2024-09-21 13:32:58 +0530

    Q1
    Q1

---
+++ Q1/IT24102488Lab6Q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class IT24102488Lab6Q1
+{
+public static void main (String[] args){
+Scanner input= new Scanner(System.in);
+
+double num = input.nextDouble();
+
+
+System.out.println("the square of "+input+" is : "+ num*num);
+System.out.println("the square root of "+ input +" is : "+ Math.sqrt(num));
+	}
+}
